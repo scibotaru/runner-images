44784898c908c3c15c2a52ce8d4b394a8afe79b0
Sergiu Cibotaru <sergiu@ireckonu.com>
2024-04-05 14:17:03 +0200

    fixes

--- images/ubuntu/scripts/tests/Tools.Tests.ps1
+++ images/ubuntu/scripts/tests/Tools.Tests.ps1
@@ -23,7 +23,48 @@ Describe "Git" {
         "git-ftp --version" | Should -ReturnZeroExitCode
     }
 }
+Describe 'Ansible' {
+    It 'Ansible' {
+        'ansible --version' | Should -ReturnZeroExitCode
+    }
+}
+Describe 'Mono' {
+    It 'mono' {
+        'mono --version' | Should -ReturnZeroExitCode
+    }
 
+    It 'msbuild' {
+        'msbuild -version' | Should -ReturnZeroExitCode
+    }
+
+    It 'nuget' {
+        'nuget' | Should -ReturnZeroExitCode
+    }
+}
+Describe 'SqlPackage' {
+    It 'sqlpackage' {
+        'sqlpackage /version' | Should -ReturnZeroExitCode
+    }
+}
+
+Describe 'Python' {
+    $testCases = @('python', 'pip', 'python3', 'pip3') | ForEach-Object { @{PythonCommand = $_ } }
+
+    It '<PythonCommand>' -TestCases $testCases {
+        "$PythonCommand --version" | Should -ReturnZeroExitCode
+    }
+}
+
+Describe 'nvm' {
+    It 'nvm' {
+        'source /etc/skel/.nvm/nvm.sh && nvm --version' | Should -ReturnZeroExitCode
+    }
+}
+Describe 'MSSQLCommandLineTools' {
+    It 'sqlcmd' {
+        'sqlcmd -?' | Should -ReturnZeroExitCode
+    }
+}
 Describe "Git-lfs" {
     It "git-lfs" {
         "git-lfs --version" | Should -ReturnZeroExitCode
@@ -49,9 +90,32 @@ Describe "Kubernetes tools" {
     }
 }
 
-Describe "Packer" {
-    It "packer" {
-        "packer --version" | Should -ReturnZeroExitCode
+Describe 'Docker' {
+    It 'docker client' {
+        $version = (Get-ToolsetContent).docker.components | Where-Object { $_.package -eq 'docker-ce-cli' } | Select-Object -ExpandProperty version
+        If ($version -ne 'latest') {
+            $(docker version --format '{{.Client.Version}}') | Should -BeLike "*$version*"
+        } else {
+            "docker version --format '{{.Client.Version}}'" | Should -ReturnZeroExitCode
+        }
+    }
+
+    It 'docker buildx' {
+        $version = (Get-ToolsetContent).docker.plugins | Where-Object { $_.plugin -eq 'buildx' } | Select-Object -ExpandProperty version
+        If ($version -ne 'latest') {
+            $(docker buildx version) | Should -BeLike "*$version*"
+        } else {
+            'docker buildx' | Should -ReturnZeroExitCode
+        }
+    }
+
+    It 'docker compose v2' {
+        $version = (Get-ToolsetContent).docker.plugins | Where-Object { $_.plugin -eq 'compose' } | Select-Object -ExpandProperty version
+        If ($version -ne 'latest') {
+            $(docker compose version --short) | Should -BeLike "*$version*"
+        } else {
+            'docker compose version --short' | Should -ReturnZeroExitCode
+        }
     }
 }
 
